531dfc5755dce2362018bd9e4eb27f644adc7dd7
david-dong06 <111911277+david-dong06@users.noreply.github.com>
2023-05-01 19:32:00 +0000

    deg

--- student.py
+++ student.py
@@ -61,13 +61,14 @@ class Piggy(PiggyParent):
     def david(self):
       print("this is for testing")
       self.deg_fwd(360)
-      self.turn_by_deg(90)
+      '''self.turn_by_deg(90)
       self.deg_fwd(360)
       self.turn_by_deg(90)
       self.deg_fwd(360)
       self.turn_by_deg(90)
       self.deg_fwd(360)
       self.turn_by_deg(90)
+      '''
       
     def safe_to_dance(self):
       
