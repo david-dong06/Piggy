ac7614736130d901a3c7063fa59888e2d32bd9b5
david-dong06 <111911277+david-dong06@users.noreply.github.com>
2023-05-11 19:18:00 +0000

    .

--- student.py
+++ student.py
@@ -82,15 +82,16 @@ class Piggy(PiggyParent):
       return True
 
     def dodge(self):
-    while True:
-      if self.searech() == True:
-        self.deg_fwd(360)
-      else:
-        self.turn_by_deg(90)
-        
-        self.deg_fwd(360)
-        
-        self.turn_by_deg(-90)
+      while True:
+        if self.searech() == True:
+          self.deg_fwd(360)
+        else:
+          self.turn_by_deg(90)
+          
+          self.fwd()
+          time.sleep(1)
+          
+          self.turn_by_deg(-90)
           
         
   
